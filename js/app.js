1b042ebdcaa0d1f5ff2cb34fc04e8ea24febb5a3
emus32 <gagaidoe1@gmail.com>
2020-01-13 21:31:16 +0100

    deleted comment from about.html

---
+++ js/app.js
@@ -0,0 +1,14 @@
+//Traversing
+const coursealist=document.querySelector('#courses-list');
+
+
+$(document).ready(function() {
+	$("ul#nav li:nth-child(1)").css("margin-right", "60px");
+	$("ul#nav li:nth-child(2)").css("margin-right", "316px");
+	$("ul#nav li:nth-child(3)").css("margin-right", "60px");
+	$("ul#nav li:nth-child(4)").css("margin-right", "0px");
+});
+
+
+
+console.log(coursealist);
